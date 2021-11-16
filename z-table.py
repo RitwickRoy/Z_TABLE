4e37b399358562118a94a62655bc5aa1b4421d02
Ritwick Roy <roy.ritwick@gmail.com>
2021-11-15 19:30:06 -0500

    Add files via upload

--- z-table.py
+++ z-table.py
@@ -14,14 +14,21 @@ Created on Sun Nov 14 21:44:48 2021
 
 import math
 
-
+#
+#  Evaluate Standard normal distribution
+#
 def norm(z):
     val_std_norm = (1.0/math.sqrt(2*math.pi))*math.exp(-(z*z)/2.0)
     return val_std_norm
-
+#
+#  Numerically integrate the normal distribution between
+#  lower limit (-ve infinity) and the specified z-value using trapezoidal rule.
+#  The numerical lower limit is set to -15.0. The interval size is 0.0001.
+#  The lower limit can be increased without compromising accuracy.
+#
 def Num_integrate(z_end):
     h = 0.0001
-    z_start = -11.0
+    z_start = -15.0
     z_int = z_end - z_start
     n = int(z_int/ h)
     sum = 0.5*norm(z_start)
@@ -31,17 +38,24 @@ def Num_integrate(z_end):
         sum = sum + norm(z)
     sum = sum + 0.5*norm(z_end)
     return sum*h
-
+#
+#  The integral of the standard normal distribution is a monotonically
+#  increase smooth function. To find the z-value given the cumulative probability,
+#  we need to find the root of the equation:
+#  integral[standard normal distribution] - cumulative probabilty = 0
+#
 def p_func(Z,prob):
     val = Num_integrate(Z) - prob
     return val
-
+#
+#  Find the root of the above function using bisection technique.
+#  The starting lower and upper limits for Z are set to -10 and 10
+#  respectively. The tolerance for convergence of the root finder
+#  is set to a small number.
 
 def find_root(prob):
     Z_L = -10.0
     Z_H = 10.0
-    V_L = p_func(Z_L,prob)
-    V_H = p_func(Z_H,prob)
     tol = 0.00000001
     diff = Z_H - Z_L
     while diff > tol:
@@ -54,15 +68,16 @@ def find_root(prob):
         diff = Z_H - Z_L
     return Z_M
 
-print("Standard normal distribution")
+print()
+print("### Standard normal distribution ###")
 u_str1 = input('for cummulative probability enter 1 | for Z-value enter 2   :')
 choice = int(u_str1)
 
 if(choice == 1):
     u_str = input('input the value of Z:  ')
     z_end = float(u_str)
-    print("cummulative probability:", Num_integrate(z_end))
+    print("cummulative probability: %.5f" % Num_integrate(z_end))
 elif(choice == 2):
     u_str = input('input the cummulative probability:  ')
     prob = float(u_str)
-    print('Z-Value:',find_root(prob))
+    print('Z-Value: %.3f' % find_root(prob))
